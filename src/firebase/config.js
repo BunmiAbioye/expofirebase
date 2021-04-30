4105c2a22a97ac4e91e9515c1da9918e7e5caa9f
BunmiAbioye <bunmioluwa@gmail.com>
2021-04-30 19:20:23 -0400

    Added firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,13 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+    apiKey: "AIzaSyBVFMNl-tbQabyrZ-YmFGhevgOz_2peaW8",
+    authDomain: "prog8110-d1639.firebaseapp.com",
+    databaseURL: "https://prog8110-d1639-default-rtdb.firebaseio.com",
+    projectId: "prog8110-d1639",
+    storageBucket: "prog8110-d1639.appspot.com",
+    messagingSenderId: "1006270361789",
+    appId: "1:1006270361789:web:37b1f2eed6f818e1d0c7d4"
 };
 
 if (!firebase.apps.length) {
